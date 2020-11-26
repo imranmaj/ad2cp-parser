8b07159bbac73d06cfcf50a9df0422cd4775b3a4
imranmaj <49664304+imranmaj@users.noreply.github.com>
2020-11-25 23:16:20 -0800

    Handle string data record the same way as others

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 # https://support.nortekgroup.com/hc/en-us/articles/360029513952-Integrators-Guide-Signature
 
-from typing import IO, Tuple, Union, Callable, List, Optional
+from typing import TextIO, Tuple, Union, Callable, List, Optional
 from enum import Enum, unique
 
 
@@ -25,29 +25,29 @@ type_with_predicate_field = Tuple[type_field_name,
 type_field = Union[type_without_predicate_field, type_with_predicate_field]
 
 
-class NoMorePacketsError(Exception):
+class NoMorePackets(Exception):
     pass
 
 
 class Ad2cpReader:
-    def __init__(self, f: IO, data_record_format_type: BurstAverageDataRecordVersion):
+    def __init__(self, f: TextIO, data_record_format_type: BurstAverageDataRecordVersion):
         self.packets = []
         while True:
             try:
                 self.packets.append(Ad2cpDataPacket(
                     f, data_record_format_type))
-            except NoMorePacketsError:
+            except NoMorePackets:
                 pass
 
 
 class Ad2cpDataPacket:
-    def __init__(self, f: IO, burst_average_data_record_version: BurstAverageDataRecordVersion):
+    def __init__(self, f: TextIO, burst_average_data_record_version: BurstAverageDataRecordVersion):
         self.burst_average_data_record_version = burst_average_data_record_version
         self._read_header(f)
         self._read_data_record(f)
 
     @staticmethod
-    def _read_exact(f: IO, total_num_bytes_to_read: int) -> bytes:
+    def _read_exact(f: TextIO, total_num_bytes_to_read: int) -> bytes:
         """
         Repeatedly reads from a stream until EOF or the correct number of bytes is reached
         """
@@ -60,52 +60,44 @@ class Ad2cpDataPacket:
         while last_bytes_read is None or (len(last_bytes_read) > 0 and len(all_bytes_read) < total_num_bytes_to_read):
             last_bytes_read = f.read(num_bytes_to_read_remaining)
             num_bytes_to_read_remaining -= len(last_bytes_read)
-            if len(last_bytes_read) > 0:
-                all_bytes_read += last_bytes_read
+            all_bytes_read += last_bytes_read
         return all_bytes_read
 
-    def _read_header(self, f: IO):
+    def _read_header(self, f: TextIO):
         raw_header = self._read_data(f, self.HEADER_FORMAT)
-        print("header checksum     ", self.checksum(raw_header[: -2]), self.header_checksum)
+        print("header checksum     ", self.checksum(
+            raw_header[: -2]), self.header_checksum)
         # don't include the last 2 bytes, which is the header checksum itself
         assert self.checksum(
             raw_header[: -2]) == self.header_checksum, "invalid header checksum"
 
-    def _read_data_record(self, f: IO):
+    def _read_data_record(self, f: TextIO):
+        print("id", self.id)
         # TODO: figure out where to send the other ids
         if self.id in (0x15, 0x16, 0x18, ):  # burst/average
             if self.burst_average_data_record_version == BurstAverageDataRecordVersion.VERSION2:
-                raw_data_record = self._read_data(f, self.BURST_AVERAGE_VERSION2_DATA_RECORD_FORMAT)
+                data_record_format = self.BURST_AVERAGE_VERSION2_DATA_RECORD_FORMAT
             elif self.burst_average_data_record_version == BurstAverageDataRecordVersion.VERSION3:
-                raw_data_record = self._read_data(f, self.BURST_AVERAGE_VERSION3_DATA_RECORD_FORMAT)
+                data_record_format = self.BURST_AVERAGE_VERSION3_DATA_RECORD_FORMAT
             else:
                 raise ValueError("invalid burst/average data record version")
-        elif self.id == 0x1c: # echosounder
+        elif self.id == 0x1c:  # echosounder
             # echosounder is only supported by burst/average v3
-            raw_data_record = self._read_data(f, self.BURST_AVERAGE_VERSION3_DATA_RECORD_FORMAT)
-        elif self.id in (0x17, 0x1b): # bottom track
-            raw_data_record = self._read_data(f, self.BOTTOM_TRACK_DATA_RECORD_FORMAT)
-        elif self.id == 0xa0: # string data
-            raw_data_record = self._read_string_data_record(f)
-            print(len(raw_data_record))
+            data_record_format = self.BURST_AVERAGE_VERSION3_DATA_RECORD_FORMAT
+        elif self.id in (0x17, 0x1b):  # bottom track
+            data_record_format = self.BOTTOM_TRACK_DATA_RECORD_FORMAT
+        elif self.id == 0xa0:  # string data
+            data_record_format = self.STRING_DATA_RECORD_FORMAT
         else:
             raise ValueError("invalid id")
 
-        print("data record checksum", self.checksum(raw_data_record), self.data_record_checksum)
+        raw_data_record = self._read_data(f, data_record_format)
+        print("data record checksum", self.checksum(
+            raw_data_record), self.data_record_checksum, end="\n\n")
         assert self.checksum(
             raw_data_record) == self.data_record_checksum, "invalid data record checksum"
 
-    def _read_string_data_record(self, f: IO):
-        self.string_id = self._read_exact(f, 1)
-        self.string_data = bytes()
-        last_byte = None
-        while last_byte is None or last_byte != b"\x00":
-            last_byte = self._read_exact(f, 1)
-            self.string_data += last_byte
-        print(self.string_data)
-        return self.string_id + self.string_data
-
-    def _read_data(self, f: IO, data_format: List[type_field]) -> bytes:
+    def _read_data(self, f: TextIO, data_format: List[type_field]) -> bytes:
         raw_bytes = bytes()
         for field_format in data_format:
             if len(field_format) == 3:
@@ -127,9 +119,9 @@ class Ad2cpDataPacket:
             else:
                 field_value = int.from_bytes(
                     raw_field, byteorder="little", signed=field_signed)
-
-            setattr(self, field_name, field_value)
-            self._postprocess(field_name)
+            if field_name is not None:
+                setattr(self, field_name, field_value)
+                self._postprocess(field_name)
 
         return raw_bytes
 
@@ -143,15 +135,16 @@ class Ad2cpDataPacket:
             self.amplitude_data_included = self.configuration & 0b0000_0010_0000_0000
             self.correlation_data_included = self.configuration & 0b0000_0001_0000_0000
         elif field_name == "num_beams_and_coordinate_system_and_num_cells":
-            self.num_cells = self._reverse_bits(
-                self.num_beams_and_coordinate_system_and_num_cells >> 6, width=10)
-            self.num_beams = self._reverse_bits(
-                self.num_beams_and_coordinate_system_and_num_cells & 0b1111, width=4)
+            self.num_cells = self.num_beams_and_coordinate_system_and_num_cells & 0b0000_0011_1111_1111
+            self.coordinate_system = (
+                self.num_beams_and_coordinate_system_and_num_cells & 0b0000_1100_0000_0000) >> 10
+            self.num_beams = (
+                self.num_beams_and_coordinate_system_and_num_cells & 0b1111_0000_0000_0000) >> 12
 
-    @staticmethod
-    def _reverse_bits(n: int, width: int) -> int:
-        b = "{:0{width}b}".format(n, width=width)
-        return int(b[:: -1], 2)
+    # @staticmethod
+    # def _reverse_bits(n: int, width: int) -> int:
+    #     b = "{:0{width}b}".format(n, width=width)
+    #     return int(b[:: -1], 2)
 
     @staticmethod
     def checksum(data: bytes) -> int:
@@ -170,12 +163,16 @@ class Ad2cpDataPacket:
 
     HEADER_FORMAT: List[type_field] = [
         ("sync", 1, False),
-        ("header_size", 1, False),
+        ("header_size_bytes", 1, False),
         ("id", 1, False),
         ("family", 1, False),
-        ("data_size", 2, False),
+        ("data_size_bytes", 2, False),
         ("data_record_checksum", 2, False),
         ("header_checksum", 2, False),
+    ]
+    STRING_DATA_RECORD_FORMAT: List[type_field] = [
+        ("id", 1, None),
+        ("string_data", lambda self: self.data_size_bytes - 1, None)
     ]
     BURST_AVERAGE_VERSION2_DATA_RECORD_FORMAT: List[type_field] = [
         ("version", 1, False),
